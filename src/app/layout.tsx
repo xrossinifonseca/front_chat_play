bcdaa415703a9bff8641519c63989d14364cdf43
Rossini fonseca <rossinidrum@outlook.com>
2023-12-05 02:46:09 -0300

    Adicionar provedores de autenticação e socket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Head from "./head";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { AuthProvider } from "@/context/AuthContext";
+import { PrivateRoute } from "@/components/privateRoute";
+import { SocketProvider } from "@/context/SocketContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,12 +28,18 @@ export default function RootLayout({
     <html lang="en">
       <Head />
       <body className={poppins.className}>
-        <main className="overflow-hidden relative bg-purple-primary">
-          <NavBar />
-          {children}
-
-          <Footer />
-        </main>
+        <AuthProvider>
+          <PrivateRoute>
+            <SocketProvider>
+              <main className="overflow-hidden relative bg-purple-primary">
+                <ToastContainer />
+                <NavBar />
+                {children}
+                <Footer />
+              </main>
+            </SocketProvider>
+          </PrivateRoute>
+        </AuthProvider>
       </body>
     </html>
   );

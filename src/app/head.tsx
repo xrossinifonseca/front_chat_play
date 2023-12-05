a65089748cb40fbf57e89f910ad2d1364d3bca5c
Rossini fonseca <rossinidrum@outlook.com>
2023-12-05 02:46:25 -0300

    Adicionar icon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>Chat play</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta content="create by rossini fonseca" name="description" />
+      <link rel="icon" href="icon.svg" />
     </head>
   );
 }

545158a7ab267ff14e45fd141c7560587e5f7bdd
Rossini fonseca <rossinidrum@outlook.com>
2023-12-06 14:08:00 -0300

    refactor: Incluir valor warning e refatora arquivo

--- src/functions/notifcations.ts
+++ src/functions/notifcations.ts
@@ -1,30 +1,35 @@
-import { toast } from "react-toastify";
+import { ToastOptions, toast } from "react-toastify";
+
+const options: ToastOptions = {
+  position: "top-center",
+  autoClose: 3000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+};
 
 export const notifcation = {
   error(message: string) {
     toast(message, {
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
+      ...options,
       theme: "colored",
       type: "error",
     });
   },
   success(message: string) {
     toast(message, {
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
+      ...options,
       theme: "colored",
       type: "success",
+    });
+  },
+  warning(message: string) {
+    toast(message, {
+      ...options,
+      theme: "colored",
+      type: "warning",
     });
   },
 };
